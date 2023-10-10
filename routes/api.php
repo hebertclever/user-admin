ca5ec3f370d7a0b96aeacc1902418608f04c6169
hebertclever <hebertclever@gmail.com>
2023-10-10 20:38:07 -0300

    metodo show

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\UserController;
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
-|
+|  Bora
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
